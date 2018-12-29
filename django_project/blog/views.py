73997c42092753cc4f229c00f5803b2908a42849
radoslawczapp <czappradoslaw@gmail.com>
2018-12-29 13:35:00 +0100

    Database and Migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author' : 'RadekJC',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'December 28, 2018'
-    },
-    {
-        'author' : 'John Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'December 29, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
