f03837fdaa856ea03e3c57607522ee13a19a6f9e
radoslawczapp <czappradoslaw@gmail.com>
2018-12-30 21:21:48 +0100

    Email and Password Reset

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -20,15 +20,14 @@ def register(request):
 @login_required
 def profile(request):
     if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST,
-                                instance=request.user)
+        u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST,
-                                    request.FILES,
-                                    instance=request.user.profile)
+                                   request.FILES,
+                                   instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account has successfully been updated!')
+            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
 
     else:
@@ -39,4 +38,5 @@ def profile(request):
         'u_form': u_form,
         'p_form': p_form
     }
+
     return render(request, 'users/profile.html', context)
