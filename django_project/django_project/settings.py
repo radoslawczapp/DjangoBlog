afd77ecbea29b234344f134ce686b39fd5c75c21
radoslawczapp <czappradoslaw@gmail.com>
2018-12-30 21:23:34 +0100

    Password

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -135,4 +135,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'czappradoslaw@gmail.com'
-EMAIL_HOST_PASSWORD = 'ctmpmpsxkgxrzvwu'
+EMAIL_HOST_PASSWORD = 'pass'
